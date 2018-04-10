7034b796f25e46a7d1ae45d3ced0e998376f125b
Eid Al-rabadi <eid@Eids-MacBook-Pro.local>
2018-04-09 21:21:42 -0400

    register

---
+++ Authenticator/app/src/main/java/com/example/eid/authenticator/Example.java
@@ -0,0 +1,13 @@
+package com.example.eid.authenticator;
+
+/**
+ * Created by eid on 3/28/18.
+ */
+
+public class Example {
+
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+
+}
---
+++ Authenticator/app/src/main/java/com/example/eid/authenticator/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.example.eid.authenticator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+
+        Button registerButton = findViewById(R.id.registerButton);
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText username = (EditText)findViewById(R.id.usernameField);
+                EditText password = (EditText)findViewById(R.id.passwordField);
+                final String sUsername = username.getText().toString();
+                final String spassword = password.getText().toString();
+                if(sUsername.matches("")){
+                    Toast.makeText(getApplicationContext(),"Please enter a username",Toast.LENGTH_SHORT).show();
+                }else if(spassword.matches("")){
+                    Toast.makeText(getApplicationContext(),"Please enter a password",Toast.LENGTH_SHORT).show();
+
+                }
+
+                // TODO Auto-generated method stub
+                else {
+                    Intent i = new Intent(RegisterActivity.this, waitingPage.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+}
